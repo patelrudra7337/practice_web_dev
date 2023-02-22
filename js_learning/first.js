024c70caa686b9365e6a1b0dfb647fabb4d9a08c
Patel Rudra <patelrudra7337@gmail.com>
2023-02-22 19:32:15 +0530

    practice_web_dev

---
+++ js_learning/first.js
@@ -0,0 +1,15 @@
+console.log("Coffee example");
+let x=12;
+let y=25;
+let q=6;
+
+z=x*y;
+const w =Math.floor(x/q);
+r=x-w;
+e=w*25;
+g=r*25;
+console.log("The price of total coffee is",z);
+console.log("The free cups that customer paid is ",w)
+console.log("The amount of total cups after offer buyed",r)
+console.log("The amount save in the offer is",e)
+console.log("The amount total customer has to paid",g)
